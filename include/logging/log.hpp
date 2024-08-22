32ad81a381a0780ac4615f76ad7b0624f608e25b
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2024-08-22 15:44:29 +0200

    logging header changed

--- include/logging/log.hpp
+++ include/logging/log.hpp
@@ -1,13 +1,115 @@
 #ifndef LOGGING_HPP_
 #define LOGGING_HPP_
 
+#include <chrono>
+#include <iomanip>
 #include <iostream>
+#include <mutex>
+#include <sstream>
+#include <string>
+#include <utils/colors.hpp>
 
-template <typename T> void LOG(T msg) { std::cout << msg << std::endl; }
+namespace tflite::logging {
+class Logger {
+public:
+  enum class Level { DEBUG, INFO, WARNING, ERROR, FATAL };
 
-template <typename T, typename... Args> void LOG(T msg, Args... args) {
-  std::cout << msg;
-  LOG(args...);
-}
+  static Logger &getInstance() {
+    static Logger instance;
+    return instance;
+  }
+
+  template <typename... Args>
+  void log(Level level, const char *file, int line, Args... args) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    std::stringstream ss;
+    ss << getColor(level) << getLevelString(level) << " "
+       << getCurrentTimestamp() << " " << file << ":" << line << " - ";
+
+    (ss << ... << args) << RESET << std::endl;
+
+    m_output << ss.str();
+    m_output.flush();
+  }
+
+  void setOutput(std::ostream &os) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    m_output.rdbuf(os.rdbuf());
+  }
+
+private:
+  Logger() : m_output(std::cout.rdbuf()) {}
+  Logger(const Logger &) = delete;
+  Logger &operator=(const Logger &) = delete;
+
+  std::string getLevelString(Level level) const {
+    switch (level) {
+    case Level::DEBUG:
+      return "[DEBUG]";
+    case Level::INFO:
+      return "[INFO]";
+    case Level::WARNING:
+      return "[WARNING]";
+    case Level::ERROR:
+      return "[ERROR]";
+    case Level::FATAL:
+      return "[FATAL]";
+    default:
+      return "[UNKNOWN]";
+    }
+  }
+
+  std::string getColor(Level level) const {
+    switch (level) {
+    case Level::DEBUG:
+      return BLUE;
+    case Level::INFO:
+      return GREEN;
+    case Level::WARNING:
+      return YELLOW;
+    case Level::ERROR:
+      return RED;
+    case Level::FATAL:
+      return MAGENTA;
+    default:
+      return RESET;
+    }
+  }
+
+  std::string getCurrentTimestamp() const {
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
+    return ss.str();
+  }
+
+  std::mutex m_mutex;
+  std::ostream m_output;
+};
+} // namespace tflite::logging
+
+#define LOG_DEBUG(...)                                                         \
+  tflite::logging::Logger::getInstance().log(                                  \
+      tflite::logging::Logger::Level::DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+
+#define LOG_INFO(...)                                                          \
+  tflite::logging::Logger::getInstance().log(                                  \
+      tflite::logging::Logger::Level::INFO, __FILE__, __LINE__, __VA_ARGS__)
+
+#define LOG_WARNING(...)                                                       \
+  tflite::logging::Logger::getInstance().log(                                  \
+      tflite::logging::Logger::Level::WARNING, __FILE__, __LINE__,             \
+      __VA_ARGS__)
+
+#define LOG_ERROR(...)                                                         \
+  tflite::logging::Logger::getInstance().log(                                  \
+      tflite::logging::Logger::Level::ERROR, __FILE__, __LINE__, __VA_ARGS__)
+
+#define LOG_FATAL(...)                                                         \
+  tflite::logging::Logger::getInstance().log(                                  \
+      tflite::logging::Logger::Level::FATAL, __FILE__, __LINE__, __VA_ARGS__)
 
 #endif // LOGGING_HPP_
