010aa29682d6c088f6287cbbcbc3bfe53950776a
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2024-08-22 15:45:00 +0200

    inference status header added

---
+++ include/utils/inference_status.hpp
@@ -0,0 +1,19 @@
+//
+// Created by arghadeep on 22.08.24.
+//
+
+#ifndef INFERENCE_STATUS_HPP
+#define INFERENCE_STATUS_HPP
+
+namespace tflite::inference {
+enum class InferenceStatus {
+  OK,
+  MODEL_LOAD_ERROR,
+  INTERPRETER_ERROR,
+  TENSOR_ALLOCATION_ERROR,
+  INVOCATION_ERROR,
+  INPUT_ERROR
+};
+} // namespace tflite::inference
+
+#endif // INFERENCE_STATUS_HPP
