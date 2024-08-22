44f186460397197e9ab3dbe19e2f3021c2a6b7db
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2024-08-22 15:44:45 +0200

    colors header added for logging

---
+++ include/utils/colors.hpp
@@ -0,0 +1,18 @@
+//
+// Created by arghadeep on 22.08.24.
+//
+#ifndef COLORS_HPP_
+#define COLORS_HPP_
+
+#define RESET "\033[0m"
+#define RED "\033[31m"     /* Red */
+#define GREEN "\033[32m"   /* Green */
+#define YELLOW "\033[33m"  /* Yellow */
+#define BLUE "\033[34m"    /* Blue */
+#define MAGENTA "\033[35m" /* Blue */
+#define CYAN "\033[36m"    /* Blue */
+#define BOLD "\033[1m"
+#define WHITE "\033[37m"
+#define UNDERLINE "\033[4m"
+
+#endif  // COLORS_HPP_
