3399e2e7eeba8b98e015b32665e672bcf872b094
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2024-09-24 15:05:32 +0200

    glogger added

--- examples/example_object_detection.cpp
+++ examples/example_object_detection.cpp
@@ -8,23 +8,41 @@
 
 #include <infer/infer.hpp>
 #include <iostream>
+#include <log/glogging.hpp>
 #include <log/log.hpp>
 #include <opencv2/opencv.hpp>
 #include <utils/scoped_timer.hpp>
 #include <visualizer/object_detection.hpp>
 
-int main() {
+int main(int argc, char **argv) {
+  tflite::logging::GLogger::init(argv[0],
+                                 std::string(PROJECT_SOURCE_DIR) + "/logs");
   std::string img_1_path =
-      std::string(PROJECT_SOURCE_DIR) + "/data/person_4.jpg";
+      std::string(PROJECT_SOURCE_DIR) + "/data/person_3.jpg";
   cv::Mat img_1 = cv::imread(img_1_path);
+  if (img_1.empty()) {
+    LOG(ERROR) << "Failed to read the image";
+    tflite::logging::GLogger::shutdown();
+    return -1;
+  } else {
+    LOG(INFO) << "Image Loaded Successfully";
+  }
 
   // Object Detection
   {
+    LOG(INFO) << "Object Detection Started";
     std::string modelPath =
-        std::string(PROJECT_SOURCE_DIR) + "/models/mobilenet_ssd_v1.tflite";
+        std::string(PROJECT_SOURCE_DIR) + "/models/mobilenet_ssd_v12.tflite";
     tflite::inference::TFLiteInferenceEngine object_detection;
 
-    object_detection.load_model(modelPath);
+    if (object_detection.load_model(modelPath) ==
+        tflite::inference::InferenceStatus::SUCCESS) {
+      LOG(INFO) << "Model Loaded Successfully";
+    } else {
+      LOG(ERROR) << "Failed to load the model";
+      tflite::logging::GLogger::shutdown();
+      return -1;
+    }
     cv::Mat img_1_clone = img_1.clone();
     cv::resize(img_1_clone, img_1_clone,
                cv::Size(object_detection.get_input_height(),
@@ -32,6 +50,7 @@ int main() {
 
     auto [output_locations, output_classes, output_scores, num_detections] =
         object_detection.infer(img_1_clone);
+    LOG(INFO) << "Inference Completed Successfully";
 
     cv::Mat overlayed_image =
         tflite::visualizer::ObjectDetectionVisualizer::overlay(
@@ -41,7 +60,11 @@ int main() {
     auto status = tflite::visualizer::ObjectDetectionVisualizer::show(
         overlayed_image, "Detection");
     if (status != tflite::visualizer::VisualizationStatus::SUCCESS) {
-      LOG_ERROR("Failed to show the image");
+      LOG(ERROR) << "Failed to show the image";
+      tflite::logging::GLogger::shutdown();
+      return -1;
     }
   }
+  tflite::logging::GLogger::shutdown();
+  return 0;
 }
